0abbca833022727c7819d5b0aab9334bc30497c9
Aldar <dondokovab@mer.ci.nsu.ru>
2024-05-13 18:43:11 +0700

    1
    1

--- app/static/login.js
+++ app/static/login.js
@@ -1,11 +1,34 @@
-function handleInput(input) {
-    const placeholderText = input.id === 'phone' ? 'Телефон' : 'Пароль';
-    const inputValue = input.value.trim();
+document.getElementById('login-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // предотвратить отправку формы
 
-    // Если введен хотя бы один символ, заменяем текст
-    if (inputValue.length > 0) {
-        input.placeholder = '';
-    } else {
-        input.placeholder = placeholderText;
+  let formData = {};
+  // собираем значения полей формы и добавляем их в объект formData
+  document.querySelectorAll('.vvod').forEach(function(input) {
+    formData[input.name] = input.value;
+  });
+
+  let jsonData = JSON.stringify(formData); // преобразуем объект в JSON строку
+
+  // отправляем данные на сервер с помощью AJAX
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/loginsubmit', true); // указываем метод POST и адрес сервера
+  xhr.setRequestHeader('Content-Type', 'application/json');
+
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      console.log('Данные успешно отправлены на сервер!');
+    // Parse the response as JSON
+      let response = JSON.parse(xhr.responseText);
+      // Check if the response indicates success
+      if (response.success) {
+      // Redirect the user to the index page
+      window.location.href = "/index";
+    }}
+    else {
+      let response = JSON.parse(xhr.responseText);
+      console.error(response.error);
+      window.location.href = "/login";
     }
-}
+  };
+  xhr.send(jsonData); // отправляем JSON данные на сервер
+});
--- app/static/registr.js
+++ app/static/registr.js
@@ -22,31 +22,42 @@ document.addEventListener('DOMContentLoaded', function() {
             errorText.style.display = 'none';
         }
     });
+});
 
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        const formData = new FormData(this);
-        
-        fetch('/registratesubmit', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if (response.ok) {
-                location.href = "./";
-                console.log('Данные успешно отправлены на сервер');
-            } else {
-                errorText.textContent = 'Ошибка при отправке данных на сервер';
-                errorText.style.display = 'block';
-                console.error('Ошибка при отправке данных на сервер');
-            }
-        })
-        .catch(error => {
-            errorText.textContent = 'Ошибка при отправке данных на сервер';
-            errorText.style.display = 'block';
-            console.error(error.message);
-        });
+// НЕ ТРОГАТЬ ВСЕ ЧТО СНИЗУ!!!
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // предотвратить отправку формы
+
+
+    let formData = {};
+    // собираем значения полей формы и добавляем их в объект formData
+    document.querySelectorAll('.vvod').forEach(function(input) {
+         if (input.name !== "confirmPassword") {
+        formData[input.name] = input.value;
+    }
     });
+
+    let jsonData = JSON.stringify(formData); // преобразуем объект в JSON строку
+
+    // отправляем данные на сервер с помощью AJAX
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/registratesubmit', true); // указываем метод POST и адрес сервера
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            location.href = "./"
+            console.log('Данные успешно отправлены на сервер!');
+            // здесь можно добавить дополнительную обработку успешной отправки данных
+        } else if (xhr.status >= 400) {
+            const passwordInput = document.getElementById('vvod password');
+            const errorMessage = document.getElementById('error-msg')
+            let response = JSON.parse(xhr.responseText);
+            console.error(response.error);
+            passwordInput.style.borderColor = 'red';
+            errorMessage.text = xhr.response.JSON.password;
+            console.error('Произошла ошибка при отправке данных на сервер.');
+        }
+    };
+    xhr.send(jsonData); // отправляем JSON данные на сервер
 });
